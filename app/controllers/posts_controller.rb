0284a186b771d54c1ad81bc8775bbb8a049d8132
Tchilo <tchiloross@gmail.com>
2022-03-29 03:19:33 +0200

    Fix rubocop errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,8 @@
 class PostsController < ApplicationController
-
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
   end
-
 
   def show
     @post = User.find(params[:user_id]).posts.find(params[:id])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def index
-  @users = User.all 
+    @users = User.all
   end
 
   def show
-@user = User.includes(:posts).find(params[:id]) 
+    @user = User.includes(:posts).find(params[:id])
   end
 end
