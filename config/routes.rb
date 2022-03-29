02d116c9f87147d67094c4c83b499d30cddf29c5
Tchilo <tchiloross@gmail.com>
2022-03-29 22:24:43 +0200

    Add comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       post "new_comment", to: "comments#create", on: :member
-      post "like_post", "likes#create", on: :member
+      post "like_post", to: "likes#create", on: :member
     end
   end
 end
