6b1aa3bc45907fc9d9d2e34ca553fdff41442684
Tchilo <tchiloross@gmail.com>
2022-03-31 01:44:27 +0200

    Add if statement to check if comment and like are added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,17 @@
 class CommentsController < ApplicationController
   def create
     post = Post.find(params[:id])
-    comment = current_user.comments.new(text: params[:text])
+    comment = current_user.comments.new(comment_params)
     comment.post = post
-    comment.save!
+    if comment.save!
+      flash[:success]= "Your comment was added"
+    else
+      flash[:error] = "Your comment was not added"
+    end
     redirect_to user_post_path
+  end
+private
+  def comment_params
+    params.permit(:text)
   end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,11 @@ class LikesController < ApplicationController
   def create
     post = Post.find(params[:id])
     like = current_user.likes.create(post: post)
-    like.save!
+    if like.save!
+      flash[:success] = 'like added'
+    else
+      flash[:error] ='like was not added'
+    end
     redirect_to user_post_url
   end
 end
