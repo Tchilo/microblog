6507c3d7a120aec90dacfd7991538ddd5cced713
Tchilo <tchiloross@gmail.com>
2022-03-31 14:46:30 +0200

    Add tests for like,comment and user models

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    user = User.new(name: 'Tom', photo: 'image.png', bio: 'I am programmer', posts_counter: 0)
+    Post.new(title: 'Hello world', text: 'Helloworld',
+             likes_counter: 0, comments_counter: 0, user: user)
+  end
+
+  before { subject.save }
+
+  it 'should return most recent comments' do
+    user = User.new(name: 'Tom', photo: 'image', bio: 'bio', posts_counter: 0)
+    subject.comments.create!(user: user, text: 'hi.')
+    subject.comments.create!(user: user, text: 'hi')
+    subject.comments.create!(user: user, text: 'hi')
+    subject.comments.create!(user: user, text: 'hi')
+    subject.comments.create!(user: user, text: 'hi')
+    subject.comments.create!(user: user, text: 'hi')
+
+    expect(subject.return_five_comments).to eql 5
+  end
 end
