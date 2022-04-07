fbf574233a2258eb29fcb69cac55c7069b91460a
Tchilo <tchiloross@gmail.com>
2022-04-07 08:26:15 +0200

    Fix log out path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
+  def after_sign_out_path_for(_resource_or_scope)
+    user_session_path
+  end
 
   protected
 
